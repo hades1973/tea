dbae7222986bbabe412afa9796d6fccc30a07749
stock <stock@stockserver.stock>
2018-08-23 20:35:36 +0800

    add a bash to produce pdf files.

---
+++ gradefinal/createpdfs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# suffix=".tex"
+# if [ ! -d "pdfs" ]; then
+# 	mkdir pdfs
+# fi
+# 
+# ./gradefinal studentsinfo.xlsx
+
+# mv *.tex pdfs/
+cd pdfs
+
+dir=$(eval pwd)
+echo $dir
+
+for file in $(ls $dir | grep .${suffix})
+do
+	xelatex $file
+	xelatex $file
+done
+echo "Finished produce pdfs"
